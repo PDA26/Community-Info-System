985f58be68463249980d4da896a490ec76e7b1b6
PDA26 <cai.xiao@northeastern.edu>
2022-04-12 01:24:13 -0400

    resident trial

---
+++ src/ui/Main.java
@@ -0,0 +1,84 @@
+package ui;
+
+import model.Community;
+import model.Resident;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Main {
+    private JPanel JPanelMain;
+    private JTabbedPane tabbedPane1;
+    private JButton addButton;
+    private JButton modifyButton;
+    private JButton deleteButton;
+    private JTable tableResident;
+    private Community residentDir;
+    public Main(){
+        addButton.addActionListener(actionEvent -> {
+            Resident p = ResidentEditor.showResidentEditor(tableResident, null);
+            if (p != null) {
+                residentDir.add(p);
+            }
+        });
+        modifyButton.addActionListener(actionEvent -> {
+            int selected = tableResident.getSelectedRow();
+            if (selected != -1) {
+                Resident p = residentDir.get(selected);
+                p = ResidentEditor.showResidentEditor(tableResident, p);
+                if (p != null) {
+                    residentDir.fireTableRowsUpdated(selected, selected);
+                }
+            }
+        });
+        deleteButton.addActionListener(actionEvent -> {
+            int selected = tableResident.getSelectedRow();
+            if (selected != -1) {
+                residentDir.delete(selected);
+                //abnormalPatientModel.notifyUpdate();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Main");
+        Main main = new Main();
+        frame.setContentPane(main.JPanelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+
+    private void createUIComponents() {
+        DefaultTableCellRenderer DateRenderer = new DefaultTableCellRenderer() {
+            @Override
+            protected void setValue(Object value) {
+                if (value instanceof Date) {
+                    this.setText(DateFormat.getDateInstance().format(value));
+                } else {
+                    super.setValue(value);
+                }
+            }
+        };
+        DefaultTableCellRenderer DateTimeRenderer = new DefaultTableCellRenderer() {
+            @Override
+            protected void setValue(Object value) {
+                if (value instanceof Date) {
+                    this.setText(DateFormat.getDateTimeInstance().format(value));
+                } else {
+                    super.setValue(value);
+                }
+            }
+        };
+
+        residentDir = new Community();
+        tableResident = new JTable(residentDir);
+        // tableResident.setDefaultRenderer(Date.class, DateRenderer);
+        tableResident.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+    }
+}
---
+++ src/ui/ResidentEditor.java
@@ -0,0 +1,79 @@
+package ui;
+
+import model.Resident;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
+
+public class ResidentEditor {
+    private JTextField textFieldName;
+    private JTextField textFieldGender;
+    private JSpinner spinnerDob;
+    private JTextField textFieldAddress;
+    private JTextField textFieldCommunity;
+    private JPanel panelResidentEditor;
+
+    public static Resident showResidentEditor(Component parent, Resident resident) {
+        ResidentEditor editor = new ResidentEditor();
+        String title;
+        if (resident == null) {
+            title = "Create Resident Info";
+        } else {
+            editor.textFieldName.setText(resident.name);
+            editor.spinnerDob.getModel().setValue(resident.dob);
+            editor.textFieldGender.setText(resident.gender);
+            editor.textFieldAddress.setText(resident.address);
+            editor.textFieldCommunity.setText(resident.community);
+            title = "Edit Person Info";
+        }
+        int selection = JOptionPane.showConfirmDialog(parent,
+                editor.panelResidentEditor,
+                title,
+                JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.PLAIN_MESSAGE);
+        if (selection == JOptionPane.OK_OPTION) {
+            if (resident == null) {
+                resident = new Resident();
+            }
+            String name = editor.textFieldName.getText();
+            String gender = editor.textFieldGender.getText();
+            Date dob = (Date) editor.spinnerDob.getValue();
+            String address = editor.textFieldAddress.getText();
+            String community = editor.textFieldCommunity.getText();
+
+            if (name.isBlank()) {
+                JOptionPane.showMessageDialog(editor.panelResidentEditor, "Name empty!");
+                return null;
+            }
+            if((gender.toLowerCase().compareTo("male") != 0)
+                    && (gender.toLowerCase().compareTo("female") != 0)){
+                JOptionPane.showMessageDialog(editor.panelResidentEditor, "Wrong input of gender!");
+                return null;
+            }
+            if (address.isBlank()) {
+                JOptionPane.showMessageDialog(editor.panelResidentEditor, "Address empty!");
+                return null;
+            }
+            if (community.isBlank()) {
+                JOptionPane.showMessageDialog(editor.panelResidentEditor, "Community empty!");
+                return null;
+            }
+
+            resident.name = name;
+            resident.gender = gender;
+            resident.dob = dob;
+            resident.address = address;
+            resident.community = community;
+            return resident;
+        } else {
+            return null;
+        }
+    }
+
+    private void createUIComponents() {
+        spinnerDob = new JSpinner(new SpinnerDateModel());
+        JSpinner.DateEditor dateEditor = new JSpinner.DateEditor(spinnerDob, "MM/dd/yyyy");
+        spinnerDob.setEditor(dateEditor);
+    }
+}
