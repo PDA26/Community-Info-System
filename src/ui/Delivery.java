82669c28cba36cab80c732d9ea51b3753d4ed0bb
PDA26 <cai.xiao@northeastern.edu>
2022-04-23 11:09:50 -0400

    deliver algo stage1

--- src/ui/Delivery.java
+++ src/ui/Delivery.java
@@ -1,6 +1,8 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Delivery {
     private JButton goBackButton;
@@ -8,7 +10,30 @@ public class Delivery {
     private JButton chooseGroceryStoreButton;
     private JButton updateOrderStatusButton;
 
+    public Delivery() {
+        updateOrderStatusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int tag = JOptionPane.showConfirmDialog(
+                        null,
+                        "Are you sure to complete this order",
+                        "Confirmation",
+                        JOptionPane.YES_NO_OPTION
+                        );
+                if(tag == 1){
+                    int selected = tableOrders.getSelectedRow();
+                    if(selected != -1){
+                        //deal with order
+                    }
+                }else{
+                    //none
+                }
+            }
+        });
+    }
+
     private void createUIComponents() {
         // TODO: place custom component creation code here
+
     }
 }
