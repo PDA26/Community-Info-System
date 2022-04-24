ef73f5ebf6f26a793367f632b3e445aba53c0a27
Zhi Yao <yao.zhi1@northeastern.edu>
2022-04-24 11:13:22 -0400

    a few modifications

---
+++ src/model/SuperMarket/orderCenter/Product.java
@@ -0,0 +1,14 @@
+package model.SuperMarket.orderCenter;
+
+/**
+ * Product
+ */
+public class Product {
+
+    private String name;
+    private String price;
+    private String Date;
+    private int quantity;
+    private String itemStatus;
+
+}
---
+++ src/model/SuperMarket/orderCenter/Supplier.java
@@ -0,0 +1,12 @@
+package model.SuperMarket.orderCenter;
+
+/**
+ * Supplier OR Supermarket: 也就是对应的超市对应不同的 Product Catalog
+ */
+public class Supplier {
+
+    private String name;
+    private String logo;
+    private String zipCode;
+
+}
