df145812fd84258aeab9626e88498baf848c8b8b
Zhi Yao <yao.zhi1@northeastern.edu>
2022-04-24 11:15:15 -0400

    new Supermarket UI 11:15 04/27

--- src/ui/SuperMarket/MainJFrameForSupermarket.java
+++ src/ui/SuperMarket/MainJFrameForSupermarket.java
@@ -4,16 +4,13 @@ package ui.SuperMarket;
 import javax.swing.*;
 import java.awt.*;
 
-public class MainJFrameForSuperMarket extends JFrame {
+public class MainJFrameForSupermarket extends JFrame {
 
-    public MainJFrameForSuperMarket(){
+    public MainJFrameForSupermarket(){
     }
 
     public void init(JPanel panel){
-        /*add(panel);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setBounds(400,400,800,800);
-        setVisible(true);*/
+
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         panel.setOpaque(true);
         //setSize(new Dimension(800, 800));
