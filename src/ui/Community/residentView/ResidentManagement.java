ba8be2495387a319e7dbeef9fb97efdaf988d99f
loulicong <lou.li@northeastern.edu>
2022-04-29 00:55:22 -0400

    update resident view

--- src/ui/Community/residentView/ResidentManagement.java
+++ src/ui/Community/residentView/ResidentManagement.java
@@ -5,11 +5,19 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.Vector;
 
 import model.Apartment;
-
+//import model.SuperMarket.orderCenter.OrderCatalog;
+//import model.SuperMarket.orderCenter.OrderItem;
+import model.CommunityData.CommunityInfo;
+import model.CommunityData.CommunityModel;
 import model.Product;
+import model.SuperMarket.wareHouse.Warehouse;
+import model.OrderData.Order;
+import model.OrderData.AptOrderCatalog;
+//import model.SuperMarket.orderCenter.ProductCatalog;
 import model.OrderData.AptOrderCatalog;
 import ui.Main;
 
@@ -22,21 +30,26 @@ public class ResidentManagement {
     private JTable tableCart;
     private JButton btnConfirm;
     private JButton btnDelete;
-    private JButton btnUpdate;
     private JTextField txtAptNo;
     private JButton btnAdd;
 
 
-    Vector<String> titlesMenu;
-    Vector<Vector<Product>> dataMenu;
-    DefaultTableModel tableModelMenu;
-
-    Vector<String> titlesCart;
-    Vector<Vector<Product>> dataCart;
-    DefaultTableModel tableModelCart;
+//    Vector<String> titlesMenu;
+//    Vector<Vector<Product>> dataMenu;
+//    DefaultTableModel tableModelMenu;
+//
+//    Vector<String> titlesCart;
+//    Vector<Vector<Product>> dataCart;
+//    DefaultTableModel tableModelCart;
 
 //    ProductCatalog productCatalog = ProductCatalog.getInstance();
 //    OrderItem order = new OrderItem();
+
+    Warehouse warehouse;
+    Order orders;
+    AptOrderCatalog aptOrderCatalog;
+    CommunityModel communityModel;
+    CommunityInfo communityInfo;
 
     int id = 0;
 
@@ -44,16 +57,34 @@ public class ResidentManagement {
 
         // panelResidentManagement.setOpaque(true);
 
-        titlesMenu = new Vector<>();
-        titlesMenu.add("Item Name");
-        titlesMenu.add("Price");
-        titlesMenu.add("Item Status");
-        titlesMenu.add("Quantity");
-        //TODO
+
+//        titlesMenu = new Vector<>();
+//        titlesMenu.add("Item Name");
+//        titlesMenu.add("Price");
+//        titlesMenu.add("Item Status");
+//        titlesMenu.add("Quantity");
+
+        String[] titleProduct = {"Item Name", "Price", "Manufacture Date", "Quantity", "Item Status"};
+        int len_row = warehouse.getRowCount();
+        int len_col = warehouse.getColumnCount();
+        String[][] dataProduct = new String[len_row][];
+
+        for (int i=0; i<len_row; i++) {
+            for (int j=0; j<len_col; j++) {
+                dataProduct[i][j] = (String) warehouse.getValueAt(i, j);
+            }
+        }
+
+        tableItem = new JTable(dataProduct, titleProduct);
+
+        String[] titleCart = {"Item Name", "Price", "Manufacture Date", "Quantity", "Item Status"};
+        String[][] dataCart = new String[5][];
+        tableCart = new JTable(dataCart, titleCart);
+
 //        dataMenu.addElement((Vector<Product>) productCatalog.getProducts());
-        tableModelMenu = new DefaultTableModel(dataMenu, titlesMenu);
-//        tableItem = new JTable(tableModelMenu);
-        tableItem = new JTable(dataMenu, titlesMenu);
+//        tableModelMenu = new DefaultTableModel(dataMenu, titlesMenu);
+////        tableItem = new JTable(tableModelMenu);
+//        tableItem = new JTable(dataMenu, titlesMenu);
 //        tableItem.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 //        JScrollPane scrollPaneMenu = new JScrollPane(tableItem);
 //        scrollPaneMenu.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -69,17 +100,21 @@ public class ResidentManagement {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                //TODO ADD ITEMS TO CART
                 int row = tableItem.getSelectedRow();
-                int len = tableItem.getColumnCount();
-                for (int i = 0; i < len; i++) {
-                    dataCart.addElement((Vector<Product>) tableItem.getValueAt(row, i));
+//                Product curr_product = warehouse.get(row);
+                int len_col = tableItem.getColumnCount();
+
+                for (int i=0; i<len_col; i++) {
+                    dataCart[row][i] = (String) tableItem.getValueAt(row, i);
                 }
 //                order.setName(String.valueOf(tableItem.getValueAt(row, 0)));
 //                order.setUnitPrice((Double) tableItem.getValueAt(row, 1));
 //                order.setPrice((Integer) tableItem.getValueAt(row, 2));
 //                order.setQuantityForOrder((Integer) tableItem.getValueAt(row, 3));
 //                OrderCatalog.getInstance().setCurrentOrder(order);
-//                tableCart = new JTable(dataCart, titlesCart);
+//                warehouse.add(curr_product);
+                tableCart = new JTable(dataCart, titleCart);
 
                 JOptionPane.showMessageDialog(panelResidentManagement, "Successfully add to cart!");
             }
@@ -89,10 +124,19 @@ public class ResidentManagement {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int row = tableCart.getSelectedRow();
+                int len_col = tableCart.getColumnCount();
+                int len_row = tableCart.getRowCount();
 
                 if (row > 0) {
-                    String item = String.valueOf(tableCart.getValueAt(row, 0));
-//                    OrderCatalog.deleteOrder(OrderCatalog.getInstance().getCurrentOrder(), item);
+                    for (int i=0; i<len_row; i++) {
+
+                        if (i==row) { i += 1; }
+                        for (int j=0; j<len_col; j++) {
+                            dataCart[i][j] = (String) tableItem.getValueAt(i, j);
+                        }
+                    }
+                    dataCart[-1] = new String[]{};
+                    tableCart = new JTable(dataCart, titleCart);
                     JOptionPane.showMessageDialog(panelResidentManagement, "Successfully deleted!");
                 } else {
                     JOptionPane.showMessageDialog(panelResidentManagement,
@@ -114,12 +158,27 @@ public class ResidentManagement {
                         break;
                     } else {
                         id += 1;
-                        SimpleDateFormat time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                        apartment.setAptNo(Apt);
+//                        SimpleDateFormat time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//                        apartment.setAptNo(Apt);
 //                        apartment.setOrderTime(String.valueOf(time));
 //                        apartment.setOrderId(id);
 //                        OrderCatalog.addOrder(Apt, order);
 //                        AptOrderCatalog.getInstance().addNewOrderItem(order);
+
+                        //TODO COMFIRM APT ORDERS
+                        int len = tableCart.getRowCount();
+                        List<Order> curr_order = null;
+
+                        for (int i=0; i<len; i++) {
+                            Product curr_product = warehouse.get(i);
+                            orders.addProduct(curr_product);
+                            curr_order.add(orders);
+                        }
+
+                        aptOrderCatalog.setList(curr_order);
+                        aptOrderCatalog.setAptNo(Apt);
+
+
                         JOptionPane.showMessageDialog(panelResidentManagement,
                                                       "Successfully confirmed!");
                         Main.gotoPanel("CommunityMain");
@@ -132,5 +191,9 @@ public class ResidentManagement {
 
     public JPanel getPanel() {
         return panelResidentManagement;
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
