eeef88ab9943d3faa2a69c676f97a7ab8c57c433
Zhi Yao <yao.zhi1@northeastern.edu>
2022-04-29 17:42:39 -0400

    supermarket ui

--- src/ui/SuperMarket/viewOrder/OrderDetails.java
+++ src/ui/SuperMarket/viewOrder/OrderDetails.java
@@ -1,8 +1,137 @@
 package ui.SuperMarket.viewOrder;
 
+import model.OrderData.AptOrderCatalog;
+import model.OrderData.CommunityOrderCatalog;
+import model.OrderData.Order;
+import model.Product;
+import ui.Main;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
 
 public class OrderDetails {
-    private JTable table1;
-    private JButton backToOrdersInterfaceButton;
+
+
+    /**
+     * table
+     */
+    Vector<Vector<Object>> data;
+    static DefaultTableModel tableModel;
+    static {
+        tableModel = new DefaultTableModel();
+    }
+
+    String[][] allOrderDetails;
+
+    /**
+     * components
+     */
+    private JTable orderDetailsJTable;
+    private JLabel nameLabel;
+    private JPanel ViewOrderDetailsJPanel;
+    private JButton backToOrderInterfaceButton;
+    private JButton updateOrderStatusButton;
+    private String communityName;
+    private String time;
+
+    private AptOrderCatalog currentAptOrderCatalog;
+
+    private void InitTable() {
+
+        data = new Vector<>();
+//        data.addAll(productModel.getData());
+        orderDetailsJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        orderDetailsJTable.setModel(tableModel);
+        tableModel.addColumn("ID");
+        tableModel.addColumn("Date");
+        tableModel.addColumn("Product Name");
+        tableModel.addColumn("Quantity");
+        tableModel.addColumn("Unit Price");
+//        tableModel.addColumn("Status");
+
+    }
+
+    public OrderDetails() {
+
+        InitTable();
+        nameLabel.setText(communityName);
+
+        populateTable();
+
+        backToOrderInterfaceButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Main.gotoPanel("ViewOrder");
+            }
+        });
+
+        updateOrderStatusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO - yes or no to change status from 'PENDING' TO 'ACCEPTED'
+                int i = JOptionPane.showConfirmDialog(ViewOrderDetailsJPanel, "Do you want to change order status?",
+                        "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                if(i == 0){
+                    //yes
+                    int row = orderDetailsJTable.getSelectedRow();
+                    allOrderDetails[row][5] = Order.OrderStatus.ACCEPTED.toString();
+
+                    populateTable();
+
+                }else{
+
+                    return;
+
+                }
+            }
+        });
+
+    }
+
+    public void populateTable() {
+        DefaultTableModel model = (DefaultTableModel) orderDetailsJTable.getModel();
+        model.setRowCount(0);
+        Object[ ] row = new Object[5];
+        allOrderDetails = currentAptOrderCatalog.getAllOrderDetails();
+        for (int a = 0; a < allOrderDetails.length; a ++) {
+            for (int i = 0; i < allOrderDetails[a].length; i ++) {
+
+                row[i] = allOrderDetails[a][i];
+
+            }
+            model.addRow(row);
+        }
+
+    }
+
+
+    public String getCommunityName() {
+        return communityName;
+    }
+
+    public void setCommunityName(String communityName) {
+        this.communityName = communityName;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setCurrentAptOrderCatalog(CommunityOrderCatalog communityOrderCatalog, String name) {
+
+        this.currentAptOrderCatalog = communityOrderCatalog.getMap().get(name);
+
+    }
+
+    public JPanel getPanel() {
+        return ViewOrderDetailsJPanel;
+    }
+
 }
--- src/ui/SuperMarket/viewOrder/ViewOrder.java
+++ src/ui/SuperMarket/viewOrder/ViewOrder.java
@@ -1,21 +1,62 @@
 package ui.SuperMarket.viewOrder;
 
+import model.OrderData.AptOrderCatalog;
+import model.OrderData.CommunityOrderCatalog;
+import model.OrderData.Order;
 import model.OrderData.OrderCenter;
+import ui.Community.communityView.communityOrder.OrderDetail;
 import ui.Main;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
 
 public class ViewOrder {
+
+    private OrderDetails orderDetails;
+
+    /**
+     * table
+     */
+    Vector<Vector<Object>> data;
+    static DefaultTableModel tableModel;
+    static {
+        tableModel = new DefaultTableModel();
+    }
+
+    /**
+     * components
+     */
     private JButton viewOrderDetailsButton;
-    private JButton updateOrderStatusButton;
-    private JTable table1;
+    private JTable orderJTable;
     private JButton backToMarketInterfaceButton;
     private JPanel viewOrderPanel;
-    private OrderCenter oc;
+
+    private OrderCenter currentOrderCenter;
+
+
+    private void InitTable() {
+
+        data = new Vector<>();
+//        data.addAll(productModel.getData());
+        orderJTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        orderJTable.setModel(tableModel);
+        tableModel.addColumn("ID");
+        tableModel.addColumn("Community");
+        tableModel.addColumn("Order Time");
+        tableModel.addColumn("Order Status");
+
+    }
 
     public ViewOrder() {
+
+        populateOrderTable();
+        currentOrderCenter = new OrderCenter();
 
         backToMarketInterfaceButton.addActionListener(new ActionListener() {
             @Override
@@ -24,30 +65,102 @@ public class ViewOrder {
             }
         });
 
-
+        orderDetails = new OrderDetails();
+        Main.addPanel(orderDetails.getPanel(), "ViewOrderDetails");
         viewOrderDetailsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //TODO - retrieve order details from selected row
-            }
-        });
 
-        updateOrderStatusButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //TODO - yes or no to change status from 'PENDING' TO 'ACCEPTED'
-                int i = JOptionPane.showConfirmDialog(viewOrderPanel, "Do you want to change order status?",
-                        "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-                if(i == 0){
-                    //yes
-                }else{
+                int row = orderJTable.getSelectedRow();
+                if (row >= 0 ) {
 
+                    String communityName = (String) tableModel.getValueAt(row, 1);
+                    Set<Map.Entry<String, CommunityOrderCatalog>> entries = currentOrderCenter.getMap().entrySet();
+                    for (Map.Entry<String, CommunityOrderCatalog> entry : entries) {
+                        CommunityOrderCatalog value = entry.getValue();
+
+                        orderDetails.setCurrentAptOrderCatalog(value, communityName);
+
+                    }
+
+                    orderDetails.setCommunityName(communityName);
+//                    orderDetails.setTime();
+                    Main.gotoPanel("ViewOrderDetails");
+
+
+                }else {
+                    JOptionPane.showMessageDialog(null, "please select the row to operate on", "hint",
+                            JOptionPane.ERROR_MESSAGE);
                 }
+
             }
         });
+
+//        updateOrderStatusButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                //TODO - yes or no to change status from 'PENDING' TO 'ACCEPTED'
+//                int i = JOptionPane.showConfirmDialog(viewOrderPanel, "Do you want to change order status?",
+//                        "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+//                if(i == 0){
+//                    //yes
+//
+//                }else{
+//
+//                }
+//            }
+//        });
     }
+
+    public void populateOrderTable() {
+
+        DefaultTableModel model = (DefaultTableModel) orderJTable.getModel();
+        model.setRowCount(0);
+
+        Map<String, CommunityOrderCatalog> communityOrderMap = currentOrderCenter.getMap();
+        Set<Map.Entry<String, CommunityOrderCatalog>> communityEntries = communityOrderMap.entrySet();
+        int ID = 1;
+        for (Map.Entry<String, CommunityOrderCatalog> communityEntry : communityEntries) {
+
+            CommunityOrderCatalog communityMap = communityEntry.getValue();
+
+            Map<String, AptOrderCatalog> aptOrders = communityMap.getMap();
+            Set<Map.Entry<String, AptOrderCatalog>> aptEntries = aptOrders.entrySet();
+            for (Map.Entry<String, AptOrderCatalog> aptEntry : aptEntries) {
+
+                String communityName = aptEntry.getKey();
+                AptOrderCatalog apt = aptEntry.getValue();
+
+                Object[ ] row = new Object[4];
+                row[0] = ID;
+                row[1] = communityName;
+                row[2] = apt.getMostRecentOrderDate().toString();
+
+                ID ++;
+                model.addRow(row);
+
+            }
+
+        }
+
+//        HashMap<String, Product> map = productModel.getProductCatalogMap();
+//        Set<Map.Entry<String, Product>> entries = map.entrySet();
+//        for (Map.Entry<String, Product> entry : entries) {
+//
+//            Product product = entry.getValue();
+//            Object[ ] row = new Object[5];
+//            row[0] = product.getName();
+//            row[1] = product.getPrice();
+//            row[2] = product.getModifiedDate();
+//            row[3] = product.getQuantity();
+//            //row[4] = product.getItemStatus();
+//            model.addRow(row);
+
+    }
+
     public void setCurrentOrderCenter(OrderCenter oc){
-        this.oc = oc;
+        this.currentOrderCenter = oc;
     }
     public JPanel getPanel() {
         return viewOrderPanel;
