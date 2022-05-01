3ab4b2101bf7e7d4ef3505b1dbf22c7a3d569b3e
Zhi Yao <yao.zhi1@northeastern.edu>
2022-05-01 10:53:03 -0400

    Community 下面需要的一些 model

--- src/model/communityAndMarketDataTest/CommunityOrderCatalog.java
+++ src/model/communityAndMarketDataTest/CommunityOrderCatalog.java
@@ -5,6 +5,14 @@ import model.SuperMarket.orderCenter.OrderItem;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 所有超市的所有订单 --- 每个超市的所有订单 ---- 社区的所有订单--------社区下面所有公寓的订单 -- 订单信息
+ *  * MasterOrderList -- OrderCenter -- CommunityOrderCatalog -- AptOrderCatalog -- Order Item
+ *
+ * 社区存储下面所有的公寓上传的的订单信息
+ * 比如现在所在的是社区 A
+ * 那么这个类下面存储的就是 A 社区下面的所有 AptOrderCatalog 里面以每一户为单位的订单
+ */
 public class CommunityOrderCatalog {
 
 //    private static CommunityOrderCatalog instance = new CommunityOrderCatalog();
