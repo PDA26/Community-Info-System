686caaa069e69f07e0a346cb22fa46b25c5b135f
Zhi Yao <yao.zhi1@northeastern.edu>
2022-05-01 10:32:18 -0400

    data model for supermarket

--- src/model/SuperMarket/orderCenter/OrderCenter.java
+++ src/model/SuperMarket/orderCenter/OrderCenter.java
@@ -1,18 +1,56 @@
 package model.SuperMarket.orderCenter;
 
-import model.Community;
+import model.communityAndMarketDataTest.Apartment;
+import model.communityAndMarketDataTest.Community;
+import model.communityAndMarketDataTest.CommunityOrderCatalog;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * Master Order List
+ * 每个超市看到的所有社区一级一级提交上来的订单
  */
 public class OrderCenter {
 
-    private HashMap<Community, ArrayList<Order>> masterOrderMap;
+//    private static OrderCenter instance = new OrderCenter();
+    public static OrderCenter getInstance() {
+        OrderCenter instance = new OrderCenter();
+        return instance;
+    }
 
-    //List<Order> list;
+    private OrderCenter() {
 
+    }
+
+    /**
+     * 按照字段查询
+     */
+//    private HashMap<String, Map<String, Map<String, OrderItem>>> orderCenterMap = new HashMap<>();
+
+//    public void addNewCommunity(Community community, Map<String, Map<String, OrderItem>> map) {
+//
+//        orderCenterMap.put(community.getCommunityName(), map);
+//
+//    }
+
+//    public HashMap<String, Map<String, Map<String, OrderItem>>> getOrderCenterMap() {
+//        return orderCenterMap;
+//    }
+
+    /**
+     * 按照对象查询
+     */
+    private HashMap<Community, Map<Apartment, Map<String, OrderItem>>> orderCenterMap = new HashMap<>();
+
+    public void addNewCommunity(Community community, Map<Apartment, Map<String, OrderItem>> map) {
+
+        orderCenterMap.put(community, map);
+
+    }
+
+    public HashMap<Community, Map<Apartment, Map<String, OrderItem>>> getOrderCenterMap() {
+        return orderCenterMap;
+    }
 }
